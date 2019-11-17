48d673bb6c1cef1bf20e31190c09d45fa59b9349
Sergei Parhomenko <zersh@mail.ru>
2019-11-17 10:09:14 +0300

    correct rel

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,8 +3,12 @@ $installerType = 'exe'
 $url = 'https://www.faststone.org/DN/FSResizerSetup43.exe'
 $silentArgs = '/S'
                                          
+$checksum = '5CB04FF4D6CDD6DF192B1D4B9664A0EA'
+
+$checksumType = 'md5'
  
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -Checksum "$checksum" -ChecksumType "$checksumType"
+
 
 
 
